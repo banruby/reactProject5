4dd97bc7d7915a38bd1dbc2308b4a7f5baf065a7
Ruby Bantock <banruby@gmail.com>
2019-02-27 14:48:25 -0500

    See ya later SCSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css'; //compiled with scss
 
+import Header from "./Header/Header.js";
+import Footer from "./Footer/Footer.js";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>THIS IS A TEST!</h1>
+        <Header />
+        <h1>BODY CONTENT</h1>
+        <Footer />
       </div>
     );
   }
