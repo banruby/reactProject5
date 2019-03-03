138e7f50575a494cd23aca0b9b03b70ea1b371e1
Ruby Bantock <banruby@gmail.com>
2019-03-03 17:12:12 -0500

    Updates

---
+++ precache-manifest.3fe510b60aabaed41a6bf057350cea54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dda166e4d518c194a48",
+    "url": "/the-library/static/js/runtime~main.0dda166e.js"
+  },
+  {
+    "revision": "e34d4326881786b25733",
+    "url": "/the-library/static/js/main.e34d4326.chunk.js"
+  },
+  {
+    "revision": "3282d2b22a1d394c52cb",
+    "url": "/the-library/static/js/2.3282d2b2.chunk.js"
+  },
+  {
+    "revision": "e34d4326881786b25733",
+    "url": "/the-library/static/css/main.8154ecb1.chunk.css"
+  },
+  {
+    "revision": "cf74b068dfa27355866d1d8bb168792f",
+    "url": "/the-library/index.html"
+  }
+];
