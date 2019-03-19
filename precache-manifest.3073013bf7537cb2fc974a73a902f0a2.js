55597b58c8ed12534855551c9f7a8cf66470147b
Ruby Bantock <banruby@gmail.com>
2019-03-19 12:14:55 -0400

    Updates

---
+++ precache-manifest.3073013bf7537cb2fc974a73a902f0a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dda166e4d518c194a48",
+    "url": "/the-library/static/js/runtime~main.0dda166e.js"
+  },
+  {
+    "revision": "8c8445e9100b7e2a0d42",
+    "url": "/the-library/static/js/main.8c8445e9.chunk.js"
+  },
+  {
+    "revision": "3282d2b22a1d394c52cb",
+    "url": "/the-library/static/js/2.3282d2b2.chunk.js"
+  },
+  {
+    "revision": "8c8445e9100b7e2a0d42",
+    "url": "/the-library/static/css/main.83145e54.chunk.css"
+  },
+  {
+    "revision": "2e605eac9c26ae917630acd63b7e5ec4",
+    "url": "/the-library/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/the-library/precache-manifest.583d601c8abf4bb27b7f1259f6ba37ed.js"
+  "/the-library/precache-manifest.3073013bf7537cb2fc974a73a902f0a2.js"
 );
 
 workbox.clientsClaim();
